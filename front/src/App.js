3d82ccf74bc59f5b8e4437ca9c847bdb27e6585d
monther123c <monthertamimi123@gmail.com>
2022-08-30 18:59:44 +0300

    ff

---
+++ front/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Main from "./component/Main";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+
+
+
+
+
+class App extends React.Component{
+ 
+
+render(){
+    return(
+        <div>
+            <Header/>
+              <Main/> 
+            <Footer/> 
+        </div>
+    )
+}
+}
+export default App;
---
+++ front/src/component/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <div>
+                &copy; monther tamimi
+            </div>
+        )
+    }
+}
+export default Footer;
